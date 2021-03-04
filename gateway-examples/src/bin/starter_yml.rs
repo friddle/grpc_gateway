04179c1e406b535777dc73eed3a89def5ace99a9
friddle <friddle@qq.com>
2021-03-04 19:08:49 +0800

    for change impelment

--- gateway-examples/src/bin/starter_yml.rs
+++ gateway-examples/src/bin/starter_yml.rs
@@ -12,7 +12,9 @@ fn main()
         .filter(|a| a != "--file=")
         .nth(1)
         .map(|s| s.to_owned())
-        .unwrap_or_else(|| "./src/bin/demo_tls.yml".to_owned());
+        .unwrap_or_else(|| "./demo.yml".to_owned())
+        .replace(&"--file=","");
+    println!("demo file location:{}",&file_location);
     let file_location=String::from(file_location);
     let ymlStarter=DispatchYamlStarter::<HttpBisServer>::parse_from_file(&file_location).run();
     let server=ymlStarter.server.as_ref().expect("");
--- grpc-gateway/src/gateway/gateway_utils.rs
+++ grpc-gateway/src/gateway/gateway_utils.rs
@@ -4,11 +4,12 @@ use std::sync::Arc;
 use crate::balance::default::DefaultLoadBalance;
 use crate::balance::LoadBalanceBox;
 use crate::LoadBalanceType;
+use crate::balance::random::RandomLoadBalance;
 
 pub fn get_all_load_balances() -> HashMap<LoadBalanceType, LoadBalanceBox> {
     let mut maps: HashMap<LoadBalanceType, LoadBalanceBox> = HashMap::new();
     maps.insert(LoadBalanceType::Default, Arc::new(Box::new(DefaultLoadBalance::new())));
-    maps.insert(LoadBalanceType::Random, Arc::new(Box::new(DefaultLoadBalance::new())));
+    maps.insert(LoadBalanceType::Random, Arc::new(Box::new(RandomLoadBalance::new())));
     maps.insert(LoadBalanceType::Robin, Arc::new(Box::new(DefaultLoadBalance::new())));
     maps.insert(LoadBalanceType::Hash, Arc::new(Box::new(DefaultLoadBalance::new())));
     maps.insert(LoadBalanceType::Weighted, Arc::new(Box::new(DefaultLoadBalance::new())));
